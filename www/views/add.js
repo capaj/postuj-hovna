ef034a3e7bc18c2228fc339ccd72e59a9689ab30
Jiří Špác <capajj@gmail.com>
2015-06-13 22:12:34 +0200

    fixed event handling

--- www/views/add.js
+++ www/views/add.js
@@ -8,10 +8,7 @@ export class Add {
 		this.route = routeConfig;
 	}
 	clickInput(){
-		setTimeout(function(){
-			$(':file').click();
-
-		});
+		$(':file').click();
 	}
 	handleOnFilesSelected(evt) {
 		this.files = [];
