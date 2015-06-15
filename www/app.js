ff3a4541ef083c934dc0dc20584756f5dced7488
Jiri Spac <capajj@gmail.com>
2015-06-15 15:33:06 +0200

    fixed #1

--- www/app.js
+++ www/app.js
@@ -2,21 +2,20 @@ import * as bootstrap from 'bootstrap';
 import Map from './components/map';
 
 export class PostujHovna{
-	constructor(){
-		console.log('aaa');
-
-	}
-	configureRouter(config, router){
-		this.router = router;
-		//config.options.pushState = true;
-		config.title = 'Sbirej.to';
-		config.map([
-			{route: [':type/:id', 'home', ''], name: 'home', moduleId: './views/home'},
-			{route: 'pridat-hovno', name: 'add-poo', moduleId: './views/add'},
-			{route: 'pridat-kos', name: 'add-bin', moduleId: './views/add'},
-			{route: 'profil', name: 'profil', moduleId: './views/profile'},
-			{route: 'about', name: 'about', moduleId: './views/about'},
-			{route: 'zebricek', name: 'leaderboards', moduleId: './views/leaderboards'}
-		]);
-	}
+  constructor(){
+    this.loaded = true;
+  }
+  configureRouter(config, router){
+    this.router = router;
+    //config.options.pushState = true;
+    config.title = 'Sbirej.to';
+    config.map([
+      {route: [':type/:id', 'home', ''], name: 'home', moduleId: './views/home'},
+      {route: 'pridat-hovno', name: 'add-poo', moduleId: './views/add'},
+      {route: 'pridat-kos', name: 'add-bin', moduleId: './views/add'},
+      {route: 'profil', name: 'profil', moduleId: './views/profile'},
+      {route: 'about', name: 'about', moduleId: './views/about'},
+      {route: 'zebricek', name: 'leaderboards', moduleId: './views/leaderboards'}
+    ]);
+  }
 }
